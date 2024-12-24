104bf8a4b8835f8b55937f8b91c278faa1af2f27
Exgyl <68525383+Exgyl@users.noreply.github.com>
2024-12-24 14:58:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '/data/cifar10'
+cifar_100_root = '/data/cifar100'
+cub_root = '/data/cub'
+aircraft_root = '/data/aircraft'
+herbarium_dataroot = '/data/herb'
+imagenet_root = '/data/ImageNet'
+
+# OSR Split dir
+osr_split_dir = '/data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+
+dino_pretrain_path = './pretrained_models/dino_vitbase16_pretrain.pth'
